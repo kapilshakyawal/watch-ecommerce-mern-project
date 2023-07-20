67258877798b3b8ac03fe5ca84ef4628515c81c2
kapilshakyawal <kapilshakyawal5@gmail.com>
2023-07-20 15:59:12 +0530

    adding body parser

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,28 @@
-require("dotenv").config()
-const express = require("express")
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
-const cors = require("cors")
-const morgan = require("morgan")
-const cookieParser= require("cookie-parser")
+const cors = require("cors");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParsr = require("body-parser");
 // morgan(':method :url :status :res[content-length] - :response-time ms')
-app.use(morgan('combined'))
-app.use(express.json())
+app.use(morgan("combined"));
+app.use(express.json());
+app.use(bodyParsr.urlencoded({extended:true}))
+app.use(bodyParsr.json({extended:true}))
 // app.use((req, res, next) => {
 //     res.header('Access-Control-Allow-Origin', 'https://watch-ecomm-frontend.onrender.com/');
 //     next();
 //   });
-app.use(cors({
-    origin: ['https://watch-ecommerce-mern-project.vercel.app/login'],
+app.use(
+  cors({
+    origin: [`${process.env.FRONTEND_URL}`],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-  }))
+  })
+);
+
 // const whitelist = ["https://watch-ecomm-frontend.onrender.com/"]
 // const corsOptions = {
 //   origin: function (origin, callback) {
@@ -30,25 +36,23 @@ app.use(cors({
 // }
 // app.use(cors(corsOptions))
 
-
-app.use(cookieParser())
+app.use(cookieParser());
 mongoose
-.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true })
-.then((result) => {
-    console.log("Db is connected successfully...")
-// If connection is successful, start the server.
-// app.listen(process.env.PORT || 7000);
-})
-.catch((error) => {
-console.log(error);
-});
-
+  .connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    console.log("Db is connected successfully...");
+    // If connection is successful, start the server.
+    // app.listen(process.env.PORT || 7000);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const user = require("./routes/userRoute");
-const product = require("./routes/productRoute")
-app.use("/",user)
-app.use("/",product)
+const product = require("./routes/productRoute");
+app.use("/", user);
+app.use("/", product);
 
 app.listen(process.env.PORT || 7000, () => {
-    console.log(`Server is running at port ${process.env.PORT}`)
-})
+  console.log(`Server is running at port ${process.env.PORT}`);
+});

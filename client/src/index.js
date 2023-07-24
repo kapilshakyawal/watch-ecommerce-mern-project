9f5cf2b36f0c4027edd55ae2cea2e118074a8e08
kapilshakyawal <kapilshakyawal5@gmail.com>
2023-07-24 14:48:10 +0530

    fa

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import Toast from './components/Toast/ToastContainer';
 
-window.BACKEND_URL ="https://watch-ecomm-backend.onrender.com"
+window.BACKEND_URL ="https://watch-ecomm-backend.onrender.com/"
 // window.BACKEND_URL ="http://localhost:7000/"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
